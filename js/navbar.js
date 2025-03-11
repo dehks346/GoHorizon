3d03817f0ff3f68b5e0114b122ec55f1a1f4ea85
henry <@>
2025-03-11 17:25:42 +0000

    home page completed

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,12 @@
-const toggleButton = document.querySelector('.navbar__toggle');
-const navbarLinks = document.querySelector('.navbar__links');
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleButton = document.querySelector(".navbar__toggle");
+    const navbarLinks = document.querySelector(".navbar__links");
 
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active');
+    toggleButton.addEventListener("click", () => {
+        if (navbarLinks.style.maxHeight) {
+            navbarLinks.style.maxHeight = null;
+        } else {
+            navbarLinks.style.maxHeight = navbarLinks.scrollHeight + "px";
+        }
+    });
 });
