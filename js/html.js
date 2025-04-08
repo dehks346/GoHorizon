a57815325eb96f2891367f428897fcce2a279d56
henry <@>
2025-04-08 15:20:34 +0100

    .

--- js/html.js
+++
@@ -1,4 +0,0 @@
-if (window.location.pathname.endsWith('.html')) {
-    // Remove .html from the URL without reloading the page
-    history.replaceState(null, document.title, window.location.pathname.replace('.html', ''));
-  }
